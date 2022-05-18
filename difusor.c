8eb6e5aaeffca4cfaa0d7f6d45ea8f85d5dee52b
Jorge Mohedano <jorge.mohedano88@gmail.com>
2022-05-19 01:49:10 +0200

    Add makefile, difusor and suscriptor

---
+++ difusor.c
@@ -0,0 +1,179 @@
+/*
+** Fichero: difusor.c
+** Autores:
+** Jorge Mohedano Sánchez DNI 70943813Z
+** Saúl Matías Jiménez
+** Usuario: i0943813
+*/
+
+/*#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>*/
+#include <sys/types.h>   /* basic system data types */
+#include <sys/socket.h>  /* basic socket definitions */
+#include <sys/time.h>    /* timeval{} for select() */
+#include <time.h>                /* timespec{} for pselect() */
+#include <netinet/in.h>  /* sockaddr_in{} and other Internet defns */
+#include <arpa/inet.h>   /* inet(3) functions */
+#include <errno.h>
+#include <fcntl.h>               /* for nonblocking */
+#include <netdb.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>    /* for S_xxx file mode constants */
+#include <sys/uio.h>             /* for iovec{} and readv/writev */
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+
+#include <net/if.h>
+
+#define INTERFAZ "eth0"
+#define GRUPOMULTICASTIPV6 "ff15::33"
+#define MAX 4096
+
+void manejadoraSIGINT(int s);
+
+int sUDP;
+
+// ./difusor "Hola que tal" ff15::33 eth0 4343 10 15
+int main(int argc, char *argv[]) {
+    
+    int interfaz;
+
+    int saltos, puerto, intervalo;
+    char interfaz_str[INET6_ADDRSTRLEN];
+    char grupo[INET6_ADDRSTRLEN];
+    //int sUDP;
+    struct sockaddr_in6 dir_difusor, dir_multicast;
+    char mensaje[MAX];
+
+    sigset_t sigset;                // Señal SIGINT
+    struct sigaction s;
+    s.sa_flags = 0;
+    s.sa_handler = manejadoraSIGINT;
+
+    bzero(&dir_difusor, sizeof(dir_difusor));
+    bzero(&dir_multicast, sizeof(dir_multicast));
+
+    //Comprobamos los parámetros
+    if(argc == 1) {
+        strcpy(mensaje, "Hola que tal");
+        strcpy(grupo, "ff15::33");
+        strcpy(interfaz_str, "eth0");
+        puerto = 4343;
+        saltos = 10;
+        intervalo = 15;
+    } else if(argc == 7) {
+        strcpy(mensaje, argv[1]);
+        strcpy(grupo, argv[2]);
+        strcpy(interfaz_str, argv[3]);
+        puerto = atoi(argv[4]);
+        saltos = atoi(argv[5]);
+        intervalo = atoi(argv[6]);
+    } else {
+        printf("Uso: ./difusor \"mensaje_a_difundir\" IPv6Multicast interfaz puerto saltos intervalo\n\n");
+        return 0;
+    }
+
+    interfaz = if_nametoindex(interfaz_str);
+
+    printf("DIFUSOR MULTICAST\n");
+    printf("- Mensaje: %s\n- Grupo: %s\n- Interfaz: %s\n- Puerto: %d\n- Saltos: %d\n- Intervalo: %d segundos\n",
+    mensaje, grupo, interfaz_str, puerto, saltos, intervalo);
+    fflush(stdout);
+
+    // Registramos la señal para la manejadora SIGINT
+    if(sigfillset(&sigset) == -1) {
+        perror("Error al registrar la señal");
+        exit(1);
+    }
+
+    if(sigdelset(&sigset, SIGINT) == -1) {
+        perror("Error al registrar la señal");
+        exit(1);
+    }
+
+    if(sigfillset(&s.sa_mask) == -1) {
+        perror("Error al registrar la señal");
+        exit(1);
+    }
+
+    if(sigaction(SIGINT, &s, NULL) == -1) {
+        perror("Error al registrar la señal");
+        exit(1);
+    }
+
+    // Creamos socket UDP
+    if((sUDP = socket(AF_INET6, SOCK_DGRAM, 0)) < 0) {
+        perror("Error al crear el socket");
+        exit(1);
+    }
+
+    // Configuramos la interfaz
+    /*interfaz_index = if_nametoindex(interfaz);
+    if(setsockopt(sUDP, IPPROTO_IPV6, IPV6_MULTICAST_IF, (char *)&interfaz, sizeof(interfaz))==-1) {
+        perror("Error al configurar interfaz");
+        exit(1);
+    }
+
+    // Configuramos el numero de saltos
+    if(setsockopt(sUDP, IPPROTO_IPV6, IPV6_MULTICAST_HOPS, &saltos, sizeof(saltos))==-1) {
+        perror("Error al configurar el TTL");
+        exit(1);
+    }*/
+
+    // Inicializamos las estructuras con nuestra IP y puerto efímero
+    dir_difusor.sin6_family = AF_INET6;
+    dir_difusor.sin6_addr = in6addr_any;
+    dir_difusor.sin6_port = 0;
+
+    dir_multicast.sin6_family = AF_INET6;
+    dir_multicast.sin6_port = htons(puerto);
+
+    // Pasamos la direccion multicast a binario
+    if(inet_pton(AF_INET6, grupo, &dir_multicast.sin6_addr) == -1) {
+        perror("Error al pasar dir_multicast a binario (inet_pton)");
+        exit(1);
+    }
+
+    // Bind a nuestra direccion
+    if(bind(sUDP, (const struct sockaddr *)&dir_difusor, sizeof(dir_difusor)) < 0) {
+        perror("Error en bind");
+        exit(1);
+    }
+
+    // Configuramos la interfaz
+    //interfaz_index = if_nametoindex(interfaz);
+    if(setsockopt(sUDP, IPPROTO_IPV6, IPV6_MULTICAST_IF, (char *)&interfaz, sizeof(interfaz))==-1) {
+        perror("Error al configurar interfaz");
+        exit(1);
+    }
+
+    // Configuramos el numero de saltos
+    if(setsockopt(sUDP, IPPROTO_IPV6, IPV6_MULTICAST_HOPS, &saltos, sizeof(saltos))==-1) {
+        perror("Error al configurar el TTL");
+        exit(1);
+    }
+
+    // Enviamos el mensaje
+    while(1) {
+        sendto(sUDP, mensaje, strlen(mensaje), 0, (struct sockaddr *)&dir_multicast, sizeof(dir_multicast));
+        sleep(intervalo);
+    }
+
+    return 0;
+}
+
+// Manejadora señal SIGINT
+void manejadoraSIGINT (int s) {
+    close(sUDP);    // Cerramos el socket UDP
+    printf("\nSIGINT capturado\n");
+    exit(0);
+}
---
+++ suscriptor.c
@@ -0,0 +1,165 @@
+/*
+** Fichero: suscriptor.c
+** Autores:
+** Jorge Mohedano Sánchez DNI 70943813Z
+** Saúl Matías Jiménez
+** Usuario: i0943813
+*/
+
+#include <sys/types.h>   /* basic system data types */
+#include <sys/socket.h>  /* basic socket definitions */
+#include <sys/time.h>    /* timeval{} for select() */
+#include <time.h>                /* timespec{} for pselect() */
+#include <netinet/in.h>  /* sockaddr_in{} and other Internet defns */
+#include <arpa/inet.h>   /* inet(3) functions */
+#include <errno.h>
+#include <fcntl.h>               /* for nonblocking */
+#include <netdb.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>    /* for S_xxx file mode constants */
+#include <sys/uio.h>             /* for iovec{} and readv/writev */
+#include <unistd.h>
+#include <sys/wait.h>
+#include <net/if.h>
+
+#define INTERFAZ "eth0"
+#define GRUPOMULTICASTIPV6 "ff15::33"
+#define MAX 4096
+
+void manejadoraSIGINT(int s);
+
+int sUDP;
+
+// ./suscriptor ff15::33 eth0 4343
+int main(int argc, char *argv[]) {
+
+    int interfaz;
+
+    int saltos, puerto, intervalo;
+    char interfaz_str[INET6_ADDRSTRLEN];
+    char grupo[INET6_ADDRSTRLEN];
+    //int sUDP;
+    struct sockaddr_in6 dir_suscriptor;
+    struct ipv6_mreq ipv6mreq;
+    char mensaje[MAX];
+
+    int r;
+	struct sockaddr_in6 dir_difusor;
+	char dir_difusor_str[INET6_ADDRSTRLEN];
+    socklen_t len;
+    len = sizeof(struct sockaddr_in6);
+
+    sigset_t sigset;                // Señal SIGINT
+    struct sigaction s;
+    s.sa_flags = 0;
+    s.sa_handler = manejadoraSIGINT;
+
+    bzero(&dir_suscriptor, sizeof(dir_suscriptor));
+    bzero(&ipv6mreq, sizeof(ipv6mreq));
+    bzero(&dir_difusor, sizeof(dir_difusor));
+
+    //Comprobamos los parámetros
+    if(argc == 1) {
+        strcpy(grupo, GRUPOMULTICASTIPV6);
+        strcpy(interfaz_str, INTERFAZ);
+        puerto = 4343;
+    } else if(argc == 4) {
+        strcpy(grupo, argv[1]);
+        strcpy(interfaz_str, argv[2]);
+        puerto = atoi(argv[3]);
+    } else {
+        printf("Uso: ./suscriptor IPv6Multicast interfaz puerto\n\n");
+        return 0;
+    }
+
+    ipv6mreq.ipv6mr_interface = if_nametoindex(interfaz_str);
+
+    printf("SUSCRIPTOR MULTICAST\n");
+    //fflush(stdout);
+    printf("- Grupo: %s\n- Interfaz: %s\n- Puerto: %d\n", grupo, interfaz_str, puerto);
+    fflush(stdout);
+
+    // Registramos la señal para la manejadora SIGINT
+    if(sigfillset(&sigset) == -1) {
+        perror("Error al registrar la señal");
+        exit(1);
+    }
+
+    if(sigdelset(&sigset, SIGINT) == -1) {
+        perror("Error al registrar la señal");
+        exit(1);
+    }
+
+    if(sigfillset(&s.sa_mask) == -1) {
+        perror("Error al registrar la señal");
+        exit(1);
+    }
+
+    if(sigaction(SIGINT, &s, NULL) == -1) {
+        perror("Error al registrar la señal");
+        exit(1);
+    }
+
+    // Creamos socket UDP
+    if((sUDP = socket(AF_INET6, SOCK_DGRAM, 0)) < 0) {
+        perror("Error al crear el socket");
+        exit(1);
+    }
+
+    // Permitimos varios suscriptores con SO_REUSEADDR
+    int permitir = 1;
+    if(setsockopt(sUDP, SOL_SOCKET, SO_REUSEADDR, &permitir, sizeof(permitir))==-1) {
+        perror("Error al activar SO_REUSEADDR (setsockopt)");
+        exit(1);
+    }
+
+    // Inicializamos las estructuras con nuestra IP y puerto efímero
+    dir_suscriptor.sin6_family = AF_INET6;
+    dir_suscriptor.sin6_addr = in6addr_any;
+    dir_suscriptor.sin6_port  = htons(puerto);
+
+    // Convertimos la direccion multicast a binario
+    if(inet_pton(AF_INET6, grupo, &ipv6mreq.ipv6mr_multiaddr) == -1) {
+        perror("Error al pasar dir_multicast a binario (inet_pton)");
+        exit(1);
+    }
+
+    // Bind a nuestra direccion
+    if(bind(sUDP, (const struct sockaddr *)&dir_suscriptor, sizeof(dir_suscriptor)) < 0) {
+        perror("Error en bind");
+        exit(1);
+    }
+
+    // Nos unimos al grupo multicast
+    if(setsockopt(sUDP, IPPROTO_IPV6, IPV6_ADD_MEMBERSHIP, &ipv6mreq, sizeof(ipv6mreq))==-1)
+    {
+        perror("Error al unirme al grupo multicast (setsockopt)");
+        exit(1);
+    }
+
+    //Recibimos los mensajes y mostramos IP del difusor
+    while(1) {
+
+	    r = recvfrom(sUDP, mensaje, MAX - 1, 0,(struct sockaddr *)&dir_difusor, &len);
+	    if(r==-1) {
+            perror("Error al reibir el mensaje (recvfrom)");
+            exit(1);
+	    } else {
+            mensaje[r]='\0';
+		    inet_ntop(AF_INET6, &(dir_difusor.sin6_addr), dir_difusor_str, INET6_ADDRSTRLEN * sizeof(char));
+		    printf("%s\n(Difusor: %s)\n", mensaje, dir_difusor_str);
+        }
+    }
+
+    return 0;
+}
+
+// Manejadora señal SIGINT
+void manejadoraSIGINT (int s) {
+    close(sUDP);    // Cerramos el socket UDP
+    printf("\nSIGINT capturado\n");
+    exit(0);
+}
